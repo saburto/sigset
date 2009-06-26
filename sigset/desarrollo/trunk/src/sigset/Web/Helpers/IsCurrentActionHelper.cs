abaf1702d9c481df978581bacb5e27777e0ab314
sevazthian <sevazthian@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-06-26 21:17:18 +0000

    Agregado action orden de trabajo y submenu izquierdo al lado izquierdo

--- sigset/desarrollo/trunk/src/sigset/Web/Helpers/IsCurrentActionHelper.cs
+++ sigset/desarrollo/trunk/src/sigset/Web/Helpers/IsCurrentActionHelper.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Web;
+using System.Web.Security;
+using System.Security.Principal;
+using System.Web.UI;
+using System.Text;
+using MvcSiteMap.Core;
+using System.Web.Routing;
 
 namespace Helpers
 {
@@ -16,6 +23,68 @@ namespace Helpers
 
             return false;
         }
+
+
+        public static string MenuIzquierda(this HtmlHelper helper, string cssName)
+        {
+            string currentControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            string currentActionName = (string)helper.ViewContext.RouteData.Values["action"];
+
+            StringBuilder sb = new StringBuilder();
+            SiteMapProvider provider = SiteMap.Provider;
+
+            HttpContextBase httpContext = new HttpContextWrapper(HttpContext.Current);
+            UrlHelper urlHelper = new UrlHelper(new RequestContext(httpContext, RouteTable.Routes.GetRouteData(httpContext)));
+
+            if (provider.RootNode.HasChildNodes)
+            {
+                foreach (SiteMapNode item in provider.RootNode.ChildNodes)
+                {
+                    if (item is MvcSiteMapNode)
+                    {
+                        MvcSiteMapNode nodeMap = (MvcSiteMapNode)item;
+                        if (nodeMap.Controller == currentControllerName)
+                        {
+                            SiteMapNode node = item;
+                            if (node.HasChildNodes)
+                            {
+                                sb.Append("<ul>");
+                                for (int i = 0; i < node.ChildNodes.Count; i++)
+                                {
+                                    if (node.ChildNodes[i] is MvcSiteMapNode)
+                                    {
+                                        MvcSiteMapNode nodeHijo = (MvcSiteMapNode)node.ChildNodes[i];
+                                        sb.Append("<li>");  
+                                        string url = nodeHijo.Url;
+                                        if (url.StartsWith("~"))
+                                        {
+                                            url = urlHelper.Content(nodeHijo.Url);
+                                        }
+                                        if (nodeHijo.Action == currentActionName)
+                                        {
+                                            sb.AppendFormat("<a style=\"color:black;font-weight: bold;\" class=\"{0}\" href=\"{0}\">{1}</a>", url, helper.Encode(nodeHijo.Title), cssName);
+                                        }
+                                        else
+                                        {
+                                            sb.AppendFormat("<a href=\"{0}\">{1}</a>", url, helper.Encode(nodeHijo.Title));
+                                        }
+
+
+
+                                        sb.Append("</li> ");
+                                    }
+                                    //sb.Append(MvcSiteMap.Core.Helpers.SiteMapHelper.SiteMap(helper, node.ChildNodes[i]));
+                                }
+                                sb.Append("</ul>");
+                            }
+                        }
+                    }
+                }
+            }
+            return sb.ToString();
+        }
     }
+
+   
 
 }
--- sigset/desarrollo/trunk/src/sigset/Web/Views/Shared/ActualAction.ascx
+++ sigset/desarrollo/trunk/src/sigset/Web/Views/Shared/ActualAction.ascx
@@ -1,13 +1,3 @@
 ﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl" %>
 
-<% if (Html.IsCurrentAction("Index", "Home")) { %>
-<li class="first active"><span class="active"></span><%= Html.ActionLink("Home", "Index", "Home") %></li>
-<% } else { %>
-<li class="first"><%= Html.ActionLink("Home", "Index", "Home") %></li>
-<% }%>
-
-<% if (Html.IsCurrentAction("About", "Home")) { %>
-<li class="active"><span class="active"></span><%= Html.ActionLink("About", "About", "Home") %></li>
-<% } else { %>
-<li><%= Html.ActionLink("About", "About", "Home") %></li>
-<% }%>
+<%=Html.Menu("", "active") %>
