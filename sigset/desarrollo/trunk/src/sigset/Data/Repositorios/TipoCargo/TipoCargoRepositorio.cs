44705566eb524ced8544c943cfdf905adc10181f
ruminita <ruminita@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-07-01 04:50:23 +0000

    Repositorios/TipoCargo Services/TipoCargo

---
+++ sigset/desarrollo/trunk/src/sigset/Data/Repositorios/TipoCargo/TipoCargoRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data.Modelo;
+
+namespace Data.Repositorios.TipoCargo
+{
+    public class TipoCargoRepositorio : Data.Repositorios.TipoCargo.ITipoCargoRepositorio 
+    {
+        sigsetEntities _data;
+        
+        public TipoCargoRepositorio()
+        {  
+            _data = new sigsetEntities();
+        }
+        public IQueryable <Tipo_Cargo> GetTipoCargo()
+        {
+            var tipo_cargo = from tc in _data.Tipo_Cargo
+                             select tc;
+            return tipo_cargo;
+        }
+
+        public Tipo_Cargo GetTipoCargoById(int id)
+        {
+            var tipo_cargo = (from tc in _data.Tipo_Cargo
+                              where tc.Id_Tipo_Cargo == id
+                              select tc).FirstOrDefault();
+            return tipo_cargo;
+        }
+             
+
+
+    }
+}
