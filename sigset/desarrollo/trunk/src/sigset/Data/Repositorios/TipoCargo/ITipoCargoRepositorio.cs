731a0db96b32fa37fc99471c6f7b3cb70e0cd663
ruminita <rumina@users.noreply.github.com>
2009-07-01 04:50:23 +0000

    Repositorios/TipoCargo Services/TipoCargo

---
+++ sigset/desarrollo/trunk/src/sigset/Data/Repositorios/TipoCargo/ITipoCargoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Data.Repositorios.TipoCargo
+{
+    public interface ITipoCargoRepositorio
+    {
+        System.Linq.IQueryable<global::Data.Modelo.Tipo_Cargo> GetTipoCargo();
+        global::Data.Modelo.Tipo_Cargo GetTipoCargoById(int id);
+    }
+}
---
+++ sigset/desarrollo/trunk/src/sigset/Services/TipoCargo/TipoCargoServicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data.Repositorios.TipoCargo;
+using Data.Modelo;
+using Services.Helpers;
+using xVal.ServerSide;
+
+namespace Services.TipoCargo
+{
+   public class TipoCargoServicio  
+    {
+       private ITipoCargoRepositorio _repo;
+       public TipoCargoServicio(ITipoCargoRepositorio repo)
+       {
+           _repo = repo;
+       }
+       public TipoCargoServicio() 
+           :this (new TipoCargoRepositorio())
+       {
+       }
+
+       public IList<Tipo_Cargo> GetTodosLosTiposCargo()
+       {
+           return _repo.GetTipoCargo().ToList();
+       }
+        
+    }
+}
