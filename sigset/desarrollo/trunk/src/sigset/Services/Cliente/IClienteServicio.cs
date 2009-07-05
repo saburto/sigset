ea6b190b99300a4600967f36df43556952398496
sevazthian <sevazthian@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-07-05 07:52:10 +0000

    Cliente Regiones

--- sigset/desarrollo/trunk/src/sigset/Services/Cliente/IClienteServicio.cs
+++ sigset/desarrollo/trunk/src/sigset/Services/Cliente/IClienteServicio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Data.Modelo;
+using System.Collections.Generic;
 namespace Services.Clientes
 {
     public interface IClienteServicio
@@ -8,5 +9,11 @@ namespace Services.Clientes
         Cliente GetClientePorRut(decimal rut);
         Cliente GetClientePorRut(decimal rut, string dv);
         Cliente GetClientePorRut(string rut, string dv);
+
+         IList<Region> GetRegiones();
+         IList<Provincia> GetProvinciasByRegion(decimal regionId);
+         IList<Provincia> GetProvinciasByRegion(decimal regionId, string nombreAlcance );
+        IList<Comuna> GetComunasByProvincia(decimal p);
+        
     }
 }
