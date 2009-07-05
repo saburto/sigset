9ec29ee6a2f1db5b979c24217c18494c9ec68826
ruminita <ruminita@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-07-05 23:03:48 +0000

    Mantenedor usuario

--- sigset/desarrollo/trunk/src/sigset/Web/Controllers/Admin/UsuarioController.cs
+++ sigset/desarrollo/trunk/src/sigset/Web/Controllers/Admin/UsuarioController.cs
@@ -53,13 +53,44 @@ namespace Web.Controllers.Admin
             var usuario = _servicio.GetUsuarioById(id);
             ViewData["listaTipos"] = _servicio.TiposUsuarios().GetSelectCampos("Id_Tipo_Usuario", "Descripcion", usuario.Tipo_Usuario.ToString());
             ViewData["listaEmpleados"] = _servicio.TodosLosEmpleados().GetSelectCampos("Rut", "Nombre", usuario.Empleado.ToString());
-            ViewData["listaPass"] = usuario.Contraseña.ToString();
             ViewData["listaRut"] = usuario.Empleado1.Rut.GetRutCompleto().ToString();
             ViewData["listaNombreEmpleado"] = usuario.Empleado1.Nombre.ToString() + " " + usuario.Empleado1.Apellido_Paterno.ToString() + " " + usuario.Empleado1.Apellido_Materno.ToString();
             return View("Editar", usuario);
         }
-      
 
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Editar(decimal id,Usuario usuario, string listaTipos,string Pass)
+        {
+            try
+            {
+
+                if (usuario != null)
+                {
+                    usuario.Contraseña = Pass;
+                    usuario.Tipo_Usuario = decimal.Parse(listaTipos);
+                    var user = _servicio.GetUsuarioById(id);
+                    if (user != null)
+                    {
+                        usuario.Empleado1 = user.Empleado1;
+                    }
+
+                }
+                _servicio.ModificarUsuario(usuario);
+                return RedirectToAction("Lista");
+            }
+            catch (RulesException e)
+            {
+                ModelState.AddRuleErrors(e.Errors);
+
+                ViewData["listaTipos"] = _servicio.TiposUsuarios().GetSelectCampos("Id_Tipo_Usuario", "Descripcion", usuario.Tipo_Usuario.ToString());
+                ViewData["listaEmpleados"] = _servicio.TodosLosEmpleados().GetSelectCampos("Rut", "Nombre", usuario.Empleado.ToString());
+                ViewData["listaRut"] = usuario.Empleado1.Rut.GetRutCompleto().ToString();
+                ViewData["listaNombreEmpleado"] = usuario.Empleado1.Nombre.ToString() + " " + usuario.Empleado1.Apellido_Paterno.ToString() + " " + usuario.Empleado1.Apellido_Materno.ToString();
+                return View(usuario);
+               
+            }
+        }
+       
         public ActionResult Crear()
         {
              ViewData["listaEmpleados"] = _servicio.TodosLosEmpleados().GetSelectCampos("Rut", "Nombre");
