e54f28215169714c3b6ef4e93a8c4326aa6e13a9
ruminita <ruminita@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-07-12 02:49:36 +0000

    orden de trabajo ambiente tecnico

---
+++ sigset/desarrollo/trunk/src/sigset/Data/Repositorios/OrdenTrabajo/OrdenTrabajoRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data.Modelo;
+
+namespace Data.Repositorios.OrdenTrabajoRepositorio
+{
+    public class OrdenTrabajoRepositorio : Data.Repositorios.OrdenTrabajo.IOrdenTrabajoRepositorio
+    {
+        sigsetEntities _data;
+        public OrdenTrabajoRepositorio()
+        {
+            _data = new sigsetEntities();
+        }
+
+        public IQueryable<Orden_Trabajo> GetTodasLasOrdenDeTrabajo()
+        {
+            var ordenes_trabajo = from ot in _data.Orden_Trabajos
+                                  select ot;
+            return ordenes_trabajo;        
+        }
+        public IQueryable<Orden_Trabajo> GetTodasLasOrdenDeTrabajoByTecnicoId(decimal id)
+        {
+            var ordenes_trabajo = from ot in _data.Orden_Trabajos
+                                  where ot.Id_Tecnico_Asignado == id
+                                  select ot;
+            return ordenes_trabajo;
+        }
+        public Detalle GetOrdenTrabajoById(decimal id)
+        {
+            var ordenes_trabajo = (from det in _data.Detalles
+                                   where det.Id_Orden == id
+                                   orderby det.Fecha_Creacion
+                                   select det).FirstOrDefault();
+            return ordenes_trabajo;
+        }
+
+    }
+}
---
+++ sigset/desarrollo/trunk/test/TestData/TestData/OrdenTrabajoRepositorioTest.cs
@@ -0,0 +1,123 @@
+﻿using Data.Repositorios.OrdenTrabajoRepositorio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Data.Modelo;
+using System.Linq;
+
+namespace TestData
+{
+    
+    
+    /// <summary>
+    ///Se trata de una clase de prueba para OrdenTrabajoRepositorioTest y se pretende que
+    ///contenga todas las pruebas unitarias OrdenTrabajoRepositorioTest.
+    ///</summary>
+    [TestClass()]
+    public class OrdenTrabajoRepositorioTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtiene o establece el contexto de la prueba que proporciona
+        ///la información y funcionalidad para la ejecución de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Atributos de prueba adicionales
+        // 
+        //Puede utilizar los siguientes atributos adicionales mientras escribe sus pruebas:
+        //
+        //Use ClassInitialize para ejecutar código antes de ejecutar la primera prueba en la clase 
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup para ejecutar código después de haber ejecutado todas las pruebas en una clase
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize para ejecutar código antes de ejecutar cada prueba
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup para ejecutar código después de que se hayan ejecutado todas las pruebas
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Una prueba de Constructor OrdenTrabajoRepositorio
+        ///</summary>
+        [TestMethod()]
+        public void OrdenTrabajoRepositorioConstructorTest()
+        {
+            OrdenTrabajoRepositorio target = new OrdenTrabajoRepositorio();
+            Assert.Inconclusive("TODO: Implementar código para comprobar el destino");
+        }
+
+        /// <summary>
+        ///Una prueba de GetOrdenTrabajoById
+        ///</summary>
+        [TestMethod()]
+        public void GetOrdenTrabajoByIdTest()
+        {
+            OrdenTrabajoRepositorio target = new OrdenTrabajoRepositorio(); // TODO: Inicializar en un valor adecuado
+            Decimal id = 4; // TODO: Inicializar en un valor adecuado
+            Detalle expected 
+            Detalle actual;
+            actual = target.GetOrdenTrabajoById(id);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Compruebe la exactitud de este método de prueba.");
+        }
+
+        /// <summary>
+        ///Una prueba de GetTodasLasOrdenDeTrabajo
+        ///</summary>
+        [TestMethod()]
+        public void GetTodasLasOrdenDeTrabajoTest()
+        {
+            OrdenTrabajoRepositorio target = new OrdenTrabajoRepositorio(); // TODO: Inicializar en un valor adecuado
+            IQueryable<Orden_Trabajo> expected = null; // TODO: Inicializar en un valor adecuado
+            IQueryable<Orden_Trabajo> actual;
+            actual = target.GetTodasLasOrdenDeTrabajo();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Compruebe la exactitud de este método de prueba.");
+        }
+
+        /// <summary>
+        ///Una prueba de GetTodasLasOrdenDeTrabajoByTecnicoId
+        ///</summary>
+        [TestMethod()]
+        public void GetTodasLasOrdenDeTrabajoByTecnicoIdTest()
+        {
+            OrdenTrabajoRepositorio target = new OrdenTrabajoRepositorio(); // TODO: Inicializar en un valor adecuado
+            Decimal id = new Decimal(); // TODO: Inicializar en un valor adecuado
+            IQueryable<Orden_Trabajo> expected = null; // TODO: Inicializar en un valor adecuado
+            IQueryable<Orden_Trabajo> actual;
+            actual = target.GetTodasLasOrdenDeTrabajoByTecnicoId(id);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Compruebe la exactitud de este método de prueba.");
+        }
+    }
+}
