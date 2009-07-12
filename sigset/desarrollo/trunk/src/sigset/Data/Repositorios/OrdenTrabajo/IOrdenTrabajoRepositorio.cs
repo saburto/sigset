fefd6e7db99f9b2a5e9ad7c44e674a5e85a46796
ruminita <rumina@users.noreply.github.com>
2009-07-12 02:49:36 +0000

    orden de trabajo ambiente tecnico

---
+++ sigset/desarrollo/trunk/src/sigset/Data/Repositorios/OrdenTrabajo/IOrdenTrabajoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Data.Repositorios.OrdenTrabajo
+{
+   public  interface IOrdenTrabajoRepositorio
+    {
+        Data.Modelo.Detalle GetOrdenTrabajoById(decimal id);
+        System.Linq.IQueryable<Data.Modelo.Orden_Trabajo> GetTodasLasOrdenDeTrabajo();
+        System.Linq.IQueryable<Data.Modelo.Orden_Trabajo> GetTodasLasOrdenDeTrabajoByTecnicoId(decimal id);
+    }
+}
