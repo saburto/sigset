cf0c36c7c872ca90169a74f24b0ff7403b994954
sevazthian <sevazthian@2d0a1e0a-31c6-11de-8fef-1f5ae417a2df>
2009-09-13 15:21:49 +0000

    Agregado...css a pagina de inicio

---
+++ sigset/desarrollo/VersionJava/trunk/sigset/src/cl/sigti/sigset/modelo/Direccion.java
@@ -0,0 +1,109 @@
+package cl.sigti.sigset.modelo;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class Direccion {
+	@Persistent
+	@PrimaryKey
+	private Long id;
+	
+	@Persistent
+	private String pais;
+	
+	@Persistent
+	private String region;
+	
+	@Persistent
+	private String ciudad;
+	
+	@Persistent
+	private String provincia;
+	
+	@Persistent
+	private String comuna;
+	
+	@Persistent
+	private String calle;
+	
+	@Persistent
+	private String numero;
+	
+	@Persistent
+	private String codigoPostal;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getComuna() {
+		return comuna;
+	}
+
+	public void setComuna(String comuna) {
+		this.comuna = comuna;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	
+}
